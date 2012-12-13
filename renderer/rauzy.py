2f9584d573061b76143b932eb3cf748eb95fac8d
Alexander Taran <alextaran@dhcp169-155-red2.yandex.net>
2012-12-13 14:00:32 +0400

    fixed screen resolution and removed old code

--- renderer/rauzy.py
+++ renderer/rauzy.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
-#SCREEN_SIZE = (1366, 768)
-SCREEN_SIZE = (1280, 800)
-
 from modules.sequence_generator import SequenceGenerator
 from modules.vector3 import Vector3
 from modules.matrix44 import Matrix44
@@ -25,20 +22,8 @@ from bigfloat import *
 
 from operator import itemgetter
 
-def sequence_to_vbos(seq):
-  v = []
-  c = []
-  dm = {'1': [1.0, 0.0, 0.0],
-        '2': [0.0, 1.0, 0.0],
-        '3': [0.0, 0.0, 1.0]}
-  cv = Vector3(0.0, 0.0, 0.0)
-  for ch in seq:
-    cv += Vector3(dm[ch])
-    c += dm[ch]
-    v += list(cv)
-  return v, c;
-
 def draw_coordinate_system(sz = 1.0):
+  glLineWidth(1.0)
   glBegin(GL_LINES)
   glColor3f(1.0, 0.0, 0.0)
   glVertex3f(0.0, 0.0, 0.0)
@@ -50,64 +35,6 @@ def draw_coordinate_system(sz = 1.0):
   glVertex3f(0.0, 0.0, 0.0)
   glVertex3f(0.0, 0.0, 1.0)
   glEnd()
-
-def gen_rauzy_vbo(iters):
-  gen = SequenceGenerator()
-  gen.addIteration('123')
-  gen.addIteration('1')
-  gen.addIteration('31')
-  gen.processIterations(iters, 3);
-  seq = gen.lastIteration()
-  v, c = sequence_to_vbos(seq)
-  return v, c
-
-def gen_rauzy4_vbo(iters):
-  gen = SequenceGenerator()
-  gen.addIteration('12')
-  gen.addIteration('13')
-  gen.addIteration('14')
-  gen.addIteration('1')
-  gen.processIterations(iters, 4);
-  seq = gen.lastIteration()
-  deltas = {'1': [1, 0, 0, 0],
-            '2': [0, 1, 0, 0],
-            '3': [0, 0, 1, 0],
-            '4': [0, 0, 0, 1]}
-  colors = {'1': [1.0, 0.0, 0.0],
-            '2': [0.0, 1.0, 0.0],
-            '3': [0.0, 0.0, 1.0],
-            '4': [1.0, 1.0, 0.0]}
-  curr = [0, 0, 0, 0]
-  v = []
-  c = []
-  for ch in seq:
-    dlt = deltas[ch]
-    curr = [curr[i]+dlt[i] for i in xrange(4)]
-    v += curr
-    c += colors[ch]
-  return v, c
-
-def project_vbo(v, m):
-  result = []
-  for i in xrange(len(v)/3):
-    vec = v[i*3+0:i*3+3]
-    result += (array(vec) * m.T).tolist()[0]
-  return result
-
-def project_vbo4(v, m):
-  result = []
-  print "projecting " + str(len(v)/4) + " points"
-  cnt = 1;
-  for i in xrange(len(v)/4):
-    vec = v[i*4+0:i*4+4]
-    vec = [BigFloat(coord, context=precision(200)) for coord in vec]
-    projected = (array(vec) * m.T).tolist()[0]
-    #print projected
-    result += map(float, projected[1:])
-    cnt +=1
-    if cnt % 1000 == 0:
-      print "projected " + str(cnt) + " points"
-  return result
 
 def count_rauzy_normals(v):
   k = 10
@@ -155,32 +82,17 @@ def resize(w, h):
 
 
 def main():
-  print "Generating rauzy sequence"
-  #v, c = gen_rauzy_vbo(15)
-  #v4, c4 = gen_rauzy4_vbo(8)
-  #normal = [v[-3], v[-2], v[-1]]
-  #rotation = complete_basis(normal)
-  #v = project_vbo(v, rotation)
-  #normal = [ 0.92209947,  0.21038206,  0.32476447]
-  #normal4 = [v4[-4], v4[-3], v4[-2], v4[-1]]
-  #l4 = sqrt(normal4[0] ** 2 + normal4[1] ** 2 + normal4[2] ** 2 + normal4[3]**2, precision(200))
-  #with precision(200):
-  #  normal4 = [BigFloat(coord, context = precision(200))/l4 for coord in normal4]
-  #rotation4 = complete_basis4(normal4)
-  #v3 = project_vbo4(v4, rotation4)
-  #print "Rendering " + str(len(v3) / 3) + " points of Rauzy fractal"
-  #print Vector3(normal).normalized()
-  #print "N4 = ", normal4
   CURRENT_POINT_SIZE = 4.0
   CURRENT_STEP = 0.001
 
   pygame.init()
-  screen = pygame.display.set_mode(SCREEN_SIZE,
+  display_info = pygame.display.Info()
+  screen = pygame.display.set_mode((display_info.current_w, display_info.current_h),
                                    HWSURFACE|OPENGL|DOUBLEBUF|FULLSCREEN)
   clock = pygame.time.Clock()
-  resize(*SCREEN_SIZE)
+  resize(display_info.current_w, display_info.current_h)
   projection_matrix = Matrix44.perspective_projection_fov(60.0/180.0*3.14159265358,
-                        float(SCREEN_SIZE[0])/SCREEN_SIZE[1], .1, 1000.)
+                        float(display_info.current_w)/display_info.current_h, .1, 1000.)
   gl_init()
 
   movement_speed = 5.0
@@ -202,7 +114,8 @@ def main():
   vbo3.setBinaryData(v3 + c4 + n4, GL_STATIC_DRAW)
   vbo3.unBind()
 
-  light_angle = 0.0
+  light_angle_phi = 0.0
+  light_angle_theta = 0.0
 
   print "Rendering ",len(c4) / 12, " points"
 
@@ -254,9 +167,12 @@ def main():
       break
     time_passed = clock.tick()
     time_passed_seconds = time_passed / 1000.0
-    light_angle += time_passed_seconds * 2.0
-    while light_angle > 2 * pi:
-      light_angle -= 2 * pi
+    light_angle_phi   += time_passed_seconds * 1.5
+    light_angle_theta += time_passed_seconds * 0.9
+    while light_angle_phi > 2 * pi:
+      light_angle_phi -= 2 * pi
+    while light_angle_theta > 2 * pi:
+      light_angle_theta -= 2 * pi
     ## handling camera
     movement_direction = 0.0
     strafe_direction   = 0.0
@@ -296,31 +212,28 @@ def main():
     simple.enableAttrArray('col', True)
     simple.enableAttrArray('nrm', True)
     simple.setUniformMat4('MVP', MVP)
-    simple.setUniformVec3('light_dir', Vector3(cos(light_angle), 0.0, sin(light_angle)))
+    light_dir = Vector3(cos(light_angle_phi) * cos(light_angle_theta),
+                        sin(light_angle_theta),
+                        sin(light_angle_phi) * sin(light_angle_theta))
+    simple.setUniformVec3('light_dir', light_dir)
     #print Vector3(cos(light_angle), 0.0, sin(light_angle))
     vbo3.bind(GL_ARRAY_BUFFER)
     glVertexAttribPointer(simple.getAttribLocation('pos'), 3, GL_FLOAT, GL_FALSE, 0, c_void_p(0))
-    #glVertexAttribPointer(simple.getAttribLocation('pos'), 3, GL_FLOAT, GL_FALSE, 0, c_void_p(len(v3)+len(c4)))
     glVertexAttribPointer(simple.getAttribLocation('col'), 3, GL_FLOAT, GL_FALSE, 0, c_void_p(len(v3)))
     glVertexAttribPointer(simple.getAttribLocation('nrm'), 3, GL_FLOAT, GL_FALSE, 0, c_void_p(len(v3)+len(c4)))
     glDrawArrays(GL_POINTS, 0, len(v3) / 12)
     vbo3.unBind()
     simple.unBind()
-    ### 4D projection
-    '''shader4.bind()
-    shader4.enableAttrArray('pos', True)
-    shader4.enableAttrArray('col', True)
-    shader4.setUniformMat4('MVP', MVP)
-    shader4.setUniform4f('project_normal', normal4[0], normal4[1], normal4[2], normal4[3])
-    shader4.setUniform1f('project_koef', 0.0)
-    vbo4.bind(GL_ARRAY_BUFFER)
-    glVertexAttribPointer(shader.getAttribLocation('pos'), 4, GL_FLOAT,
-                          GL_FALSE, 0, c_void_p(0))
-    glVertexAttribPointer(shader.getAttribLocation('col'), 3, GL_FLOAT,
-                          GL_FALSE, 0, c_void_p(len(v4) * 4))
-    #glDrawArrays(GL_POINTS, 0, len(v4) / 3)
-    vbo4.unBind()
-    shader4.unBind()'''
+
+    #glPointSize(25.0)
+    glLineWidth(15.0)
+    glBegin(GL_LINES)
+    glColor3f(1.0, 1.0, 1.0)
+    light_R1 = 4.0
+    light_R2 = 3.0
+    glVertex3f(light_R1 * light_dir.x, light_R1 * light_dir.y, light_R1 * light_dir.z)
+    glVertex3f(light_R2 * light_dir.x, light_R2 * light_dir.y, light_R2 * light_dir.z)
+    glEnd()
     ### final flip
     pygame.display.flip()
 
